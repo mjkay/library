51d703fe6032ef4afab3e6f1c821c25cc0d0c962
Ignacio Cases <ignacio.cases@gmail.com>
2014-09-26 12:02:57 -0700

    Changes to Scala 2.11.2

--- project/LibraryBuild.scala
+++ project/LibraryBuild.scala
@@ -13,11 +13,11 @@ object LibraryBuild extends Build {
       name := "Library",
       organization := "edu.mjkay",
       version := "0.1-SNAPSHOT",
-      scalaVersion := "2.10.3",
+      scalaVersion := "2.11.2",
       // add other settings here
       libraryDependencies ++= Seq(
-        "org.scalatest" % "scalatest_2.10" % "2.0" % "test" withSources() withJavadoc(),
-        "org.scalacheck" %% "scalacheck" % "1.10.0" % "test" withSources() withJavadoc()
+        "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test" withSources() withJavadoc(),
+        "org.scalacheck" %% "scalacheck" % "1.11.5" % "test" withSources() withJavadoc()
       )
     )
   )
